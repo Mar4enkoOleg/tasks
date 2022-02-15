da436391bfd1c9ac192dbe65f3ac8243ab8d775f
Oleg Marchenko <rosstofor@gmail.com>
2022-02-15 10:53:16 +0200

    Add signUp feature

---
+++ src/auth/auth.service.ts
@@ -0,0 +1,14 @@
+import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { AuthCredentialsDto } from './dto/auth-credentials.dto';
+import { UsersRepository } from './users.repository';
+
+@Injectable()
+export class AuthService {
+  constructor(
+    @InjectRepository(UsersRepository) private usersRepository: UsersRepository,
+  ) {}
+  signUp(authCredentials: AuthCredentialsDto): Promise<void> {
+    return this.usersRepository.createUser(authCredentials);
+  }
+}
