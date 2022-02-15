d0f93dae3b34399932834bd83671d18b69004426
Oleg Marchenko <rosstofor@gmail.com>
2022-02-15 11:11:46 +0200

    Add signUp validation

--- src/auth/dto/auth-credentials.dto.ts
+++ src/auth/dto/auth-credentials.dto.ts
@@ -1,4 +1,13 @@
+import { IsString, MaxLength, MinLength } from 'class-validator';
+
 export class AuthCredentialsDto {
+  @IsString({ message: 'Username must be a string' })
+  @MinLength(4, { message: 'Username length < 4' })
+  @MaxLength(20, { message: 'Username length > 20' })
   username: string;
+
+  @IsString({ message: 'Password must be a string' })
+  @MinLength(3, { message: 'Password length < 3' })
+  @MaxLength(20, { message: 'Password length > 20' })
   password: string;
 }
